fbcafb79e74bf7f17c5b92142532efce4afc0a7f
Karimov R <rkarimov.r@gmail.com>
2018-03-05 22:07:22 +0400

    Implement pointcut execution order

---
+++ spring-aop-pointcut-order-aspects/src/main/java/aopdemo/MainDemoApp.java
@@ -0,0 +1,29 @@
+package aopdemo;
+
+import aopdemo.dao.AccountDAO;
+import aopdemo.dao.MembershipDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainDemoApp {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(DemoConfig.class);
+
+        AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+        Account account = new Account();
+        accountDAO.addAccount(account, true);
+        accountDAO.doWork();
+
+        accountDAO.setName("foobar");
+        accountDAO.setServiceCode("silver");
+
+        String name = accountDAO.getName();
+        String code = accountDAO.getServiceCode();
+
+        MembershipDAO membershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+        membershipDAO.addSillyMember();
+        membershipDAO.gotoSleep();
+
+        context.close();
+    }
+}
