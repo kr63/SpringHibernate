5b3c963f5ab438c6c2e06573b553a6f3342720d4
Karimov R <rkarimov.r@gmail.com>
2018-03-07 00:28:22 +0400

    Create simple Spring Security demo application

---
+++ spring-security/src/main/java/spring/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package spring.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer
+        extends AbstractSecurityWebApplicationInitializer{
+}
